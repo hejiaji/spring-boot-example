17e4b52907a3aff6465a9a670c4c755c4deb3aa2
jiajihe <83583245@qq.com>
2018-04-25 16:16:47 +0800

    [jeremy] implement sample controller

---
+++ src/main/java/com/example/controllers/PersonController.java
@@ -0,0 +1,29 @@
+package com.example.controllers;
+
+import com.example.entities.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping(value = "/api/person")
+public class PersonController {
+    List<Person> personList = new ArrayList<>();
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    Person getPerson(@RequestParam("id") Long id) throws Exception {
+        return personList.stream().filter(item -> id.equals(item.getId()))
+                .findFirst().orElseThrow(() -> new Exception("no element"));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    Person createPerson(@RequestBody Person person) {
+        personList.add(person);
+        return person;
+    }
+}
